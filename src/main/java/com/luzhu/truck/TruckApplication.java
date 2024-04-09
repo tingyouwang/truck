a6e02ef33a8c7a401bac588505ca3377e9a1d90c
tingYou <s3251994@gmail.com>
2024-04-10 00:43:42 +0800

    first commit

---
+++ src/main/java/com/luzhu/truck/TruckApplication.java
@@ -0,0 +1,13 @@
+package com.luzhu.truck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckApplication.class, args);
+	}
+
+}
