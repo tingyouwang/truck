691be173a49ce746e3e81f796241b8cc0f29bae7
tingYou <s3251994@gmail.com>
2024-05-25 15:45:48 +0800

    add @EnableScheduling

--- src/main/java/com/luzhu/truck/TruckApplication.java
+++ src/main/java/com/luzhu/truck/TruckApplication.java
@@ -2,8 +2,10 @@ package com.luzhu.truck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TruckApplication {
 
 	public static void main(String[] args) {
