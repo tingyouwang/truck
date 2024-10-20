5d05fc01dfebfa6fa31dc311d2fd044ae4ef3a92
tingYou <s3251994@gmail.com>
2024-10-20 17:56:38 +0800

    [MODIFY] AOP轉換民國日期TO西元

--- src/main/java/com/luzhu/truck/util/DateTimeUtil.java
+++ src/main/java/com/luzhu/truck/util/DateTimeUtil.java
@@ -1,9 +1,13 @@
 package com.luzhu.truck.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+@Slf4j
 public class DateTimeUtil {
     public static LocalDate getMonthFirst(String billDate) {
         return YearMonth.parse(billDate, DateTimeFormatter.ofPattern("yyyy-MM")).atDay(1);
@@ -24,6 +28,31 @@ public class DateTimeUtil {
         DateTimeFormatter minguoFormatter = DateTimeFormatter.ofPattern("yyy-MM-dd")
                 .withChronology(java.time.chrono.MinguoChronology.INSTANCE);
 
-        return LocalDate.parse(date, minguoFormatter).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String result = tryParseFullDate(date, minguoFormatter);
+
+        if (result.isEmpty()) {
+            result = tryParseYearMonth(date, minguoFormatter);
+        }
+
+        return result;
+    }
+    private static String tryParseYearMonth(String date, DateTimeFormatter inputFormatter ) {
+        String forFormatDate = date + "-01";
+        try {
+            LocalDate localDate = LocalDate.parse(forFormatDate, inputFormatter);
+            return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM"));
+        } catch (DateTimeParseException e) {
+            log.error(String.format("input 日期格式錯誤:%s, 自行拼接日期01:%s", date, forFormatDate));
+            throw e;
+//            return "";
+        }
+    }
+    private static String tryParseFullDate(String date, DateTimeFormatter inputFormatter) {
+        try {
+            LocalDate localDate = LocalDate.parse(date, inputFormatter);
+            return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } catch (DateTimeParseException e) {
+            return "";
+        }
     }
 }
