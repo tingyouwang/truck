84da44a4812d38e2d949ecf8acddf1cf26c8423d
tingYou <s3251994@gmail.com>
2024-12-09 12:46:27 +0800

    fix 借款利率

--- src/main/java/com/luzhu/truck/service/lendmoney/LendMoneyService.java
+++ src/main/java/com/luzhu/truck/service/lendmoney/LendMoneyService.java
@@ -1,9 +1,12 @@
 package com.luzhu.truck.service.lendmoney;
 
+import com.luzhu.truck.dao.carfee.CarFeeDao;
 import com.luzhu.truck.dao.lendmoney.LendMoneyDao;
 import com.luzhu.truck.dto.LicenseAndExpenseYearMonthParam;
+import com.luzhu.truck.dto.car.CarFeeJoinInvoiceDto;
 import com.luzhu.truck.dto.lendmoney.AddLendMoneyParam;
 import com.luzhu.truck.dto.lendmoney.UpdateLendMoneyParam;
+import com.luzhu.truck.entity.carfee.CarFee;
 import com.luzhu.truck.entity.lendmoney.LendMoney;
 import com.luzhu.truck.exception.AppException;
 import com.luzhu.truck.exception.SystemExceptionEnum;
@@ -15,6 +18,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -23,11 +27,20 @@ import java.util.List;
 public class LendMoneyService {
     @Autowired
     private LendMoneyDao lendMoneyDao;
+    @Autowired
+    private CarFeeDao carFeeDao;
     @Transactional
     public void addLendMoney(AddLendMoneyParam param) {
         long l = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+
+        CarFee carFee = carFeeDao.getCarFeeByLicenseNum(param.getCarLicenseNum());
+
+//        借款利率
+        Double taxPercent = carFee.getOweTax();
+        BigDecimal taxAmount = param.getAmount().multiply(BigDecimal.valueOf(taxPercent));
+
         int insertCount = lendMoneyDao.insertLendMoney(param.getCarLicenseNum(), param.getLendDate(), param.getAmount(), param.getType(), param.getExpireDate(),
-                param.getInterestAmount(), param.getNote(), l, l);
+                taxAmount, param.getNote(), l, l);
 
         Validator.isFalseThrow(1 == insertCount,
                 new AppException(SystemExceptionEnum.UPDATE_ERROR));
@@ -36,8 +49,13 @@ public class LendMoneyService {
     @Transactional
     public void updateLendMoney(UpdateLendMoneyParam param) {
         long l = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+
+        CarFee carFee = carFeeDao.getCarFeeByLicenseNum(param.getCarLicenseNum());
+        Double taxPercent = carFee.getOweTax();
+        BigDecimal taxAmount = param.getAmount().multiply(BigDecimal.valueOf(taxPercent));
+
         int updateCount = lendMoneyDao.updateLendMoney(param.getCarLicenseNum(), param.getLendDate(), param.getAmount(), param.getType(),
-                param.getExpireDate(), param.getInterestAmount(), param.getNote(), l, param.getId());
+                param.getExpireDate(), taxAmount, param.getNote(), l, param.getId());
 
         Validator.isFalseThrow(1 == updateCount,
                 new AppException(SystemExceptionEnum.UPDATE_ERROR));
